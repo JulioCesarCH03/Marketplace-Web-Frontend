0e9894089532c51a76537cce1a253751c592ca73
JulioCesarCH03 <juliocampoverdehuaman@gmail.com>
2025-09-23 23:10:23 -0500

    frontend

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // Puerto típico de Vite (puedes cambiarlo a 3000 si prefieres)
+    host: 'localhost',
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
